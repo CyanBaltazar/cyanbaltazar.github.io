68929c470d5a45eb0587d6db75804910807c2398
Christian Ryan T. Baltazar <technodeveloper15@gmail.com>
2018-06-01 16:24:01 +0800

    updated to bootstrap 4 and some content (social media section)

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -9,7 +9,7 @@
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $(document).on('click', 'a.page-scroll', function(event) {
+    $(document).on('click', 'button.page-scroll', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
